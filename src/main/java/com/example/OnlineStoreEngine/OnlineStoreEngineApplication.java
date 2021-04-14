e72edb11d6f58e17c736783af564e6175cd03f05
EugeniySichev <sichev91@inbox.ru>
2021-04-14 17:45:31 +0300

    Initial commit

---
+++ src/main/java/com/example/OnlineStoreEngine/OnlineStoreEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnlineStoreEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OnlineStoreEngine/OnlineStoreEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnlineStoreEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
